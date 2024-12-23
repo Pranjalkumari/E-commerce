fd050c0eeb9c8d65b625d0d5bcc8100e16ffdee1
Shubham Sengar <shubhamsengar2303@gmail.com>
2024-12-23 16:05:57 +0530

    created admin dashboard and admin authentication

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken'
 
 
-const adminAuth = async (req, resizeBy, next)=>
+const adminAuth = async (req, res, next)=>
 {
     try {
         const {token} = req.headers
-        if(!error){
+        if(!token){
             return res.json({success:false, message:"Not Authorized Login Again"})
         }
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
