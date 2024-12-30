63f56173a6f0003f455e6eda2f0f72764d60f04a
Shubham Sengar <shubhamsengar2303@gmail.com>
2024-12-30 15:06:23 +0530

    made order functionality

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import {userOrder, updateStatus,allOrders,placeOrder,placeOrderRazorpay,placeOrderStripe, verifyStripe} from "../controllers/orderController.js"
+import adminAuth from "../middleware/adminAuth.js"
+import authUser from "../middleware/auth.js"
+
+const orderRouter = express.Router()
+
+
+// Admin features
+orderRouter.post('/list',adminAuth,allOrders)
+orderRouter.post('/staus',adminAuth,updateStatus)
+
+
+// Payment Features
+orderRouter.post('/place',authUser,placeOrder)
+orderRouter.post('/stripe',authUser,placeOrderStripe)
+orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
+
+// User features
+orderRouter.post('/userorders',authUser,userOrder)
+
+
+// verify Payment
+orderRouter.post('/verifyStripe',authUser,verifyStripe)
+
+export default orderRouter;
+
+
